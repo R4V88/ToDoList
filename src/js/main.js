7f83f224b652ada0d0fe06ec78d5b30b2f40a06a
Rafal <sochacki.rfl@gmail.com>
2019-11-13 23:06:50 +0100

    czemu nie dziala? xD

--- src/js/main.js
+++ src/js/main.js
@@ -22,41 +22,61 @@ if ('serviceWorker' in navigator) {
 
 let addForm = document.querySelector('.addForm');
 let todolist = document.querySelector('.todo');
+let listitems = todolist.getElementsByTagName('todo__item');
 
 
 addForm.addEventListener('submit', addItem);
 
+let finalPos;
+function position () {
+  for (let i = 0; i < listitems.length; i++) {
+    finalPos += i;
+  }
+  return finalPos;
+}
+
+
 function addItem(e) {
   e.preventDefault();
+
+  position();
+
 
   let title = document.getElementById('addForm__title').value;
   let content = document.getElementById('addForm__content').value;
 
   let createLi = document.createElement('li');
   createLi.classList.add('todo__item');
-  
+  createLi.setAttribute('id', 'todo__item');
+
   let createDiv = document.createElement('div');
   createDiv.classList.add('todo__wrapper');
 
+  let createPos = document.createElement('p');
+  createPos.className = 'todo__position';
+  createPos.appendChild(document.createTextNode(finalPos));
+
   let createH1 = document.createElement('h1');
   createH1.classList.add('todo__title');
-  // createH1.appendChild(title);
+  createH1.appendChild(document.createTextNode(title));
 
   let createBtn = document.createElement('button');
   createBtn.classList.add('todo__delete');
-  // createBtn.appendChild(document.createTextNode('X'));
+  createBtn.appendChild(document.createTextNode('X'));
 
   let createP = document.createElement('p');
   createP.classList.add('todo__content');
-  // createP.appendChild(content);
+  createP.appendChild(document.createTextNode(content));
 
-  createLi.appendChild()
+  createDiv.appendChild(createH1);
+  createDiv.appendChild(createBtn);
 
-  console.log(createLi);
-  console.log(createDiv);
-  // console.log(createBtn);
-  // console.log(createH1);
-  // console.log(createP);
+  createLi.appendChild(createDiv);
+  createLi.appendChild(createP);
+
+  todolist.appendChild(createLi);
+
+  console.log(createPos);
+  console.log(position());
 }
-
 
